bd72ac219843b2c32ce167868abbb3780b7bff95
Alexandr <alekspetrofint@gmail.com>
2021-06-14 19:54:44 +0300

    fix adminModule route

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,12 +1,12 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { RouterModule } from "@angular/router";
-import { ProductPageComponent } from "../product-page/product-page.component";
 import { AddPageComponent } from "./add-page/add-page.component";
 import { DashboardPageComponent } from "./dashboard-page/dashboard-page.component";
 import { LoginPageComponent } from "./login-page/login-page.component";
 import { OrdersPageComponent } from "./orders-page/orders-page.component";
 import { AdminLayoutComponent } from "./shared/admin-layout/admin-layout.component";
+import { EditPageComponent } from './edit-page/edit-page.component';
 
 @NgModule({
     declarations: [
@@ -15,7 +15,7 @@ import { AdminLayoutComponent } from "./shared/admin-layout/admin-layout.compone
         DashboardPageComponent,
         AddPageComponent,
         OrdersPageComponent,
-        ProductPageComponent
+        //   ProductPageComponent
     ],
     imports: [
         CommonModule,
@@ -28,12 +28,12 @@ import { AdminLayoutComponent } from "./shared/admin-layout/admin-layout.compone
                         { path: 'dashboard', component: DashboardPageComponent },
                         { path: 'add', component: AddPageComponent },
                         { path: 'orders', component: OrdersPageComponent },
-                        { path: 'product/:id/edit', component: ProductPageComponent },
+                        { path: 'product/:id/edit', component: EditPageComponent },
                     ]
                 }
             ])
     ],
-    exports: [RouterModule ]
+    exports: [RouterModule]
 })
 export class AdminModule {
 
